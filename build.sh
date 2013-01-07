539848a0ce8f66392c1857ceb7e1c6ffa8361b7b
Robert Abraham <robert@adeven.com>
2013-01-07 14:32:43 +0100

    lesser aggressive compiler flags

--- build.sh
+++ build.sh
@@ -9,17 +9,17 @@ INCLUDEDIR=`pg_config --includedir-server`
 LIBDIR=`pg_config --libdir`
 WORKDIR=`pwd`
 
-gcc -I $INCLUDEDIR -fpic -c count.c avltree.c -I $LIBDIR -lhstore -O3 -march=native
+gcc -I $INCLUDEDIR -fpic -c count.c avltree.c -I $LIBDIR -lhstore -O2 -march=native
 gcc -shared -o count.so count.o avltree.o
 psql -U postgres -h localhost $DB -c "CREATE OR REPLACE FUNCTION welle_count(anyarray) RETURNS hstore AS '$WORKDIR/count.so' LANGUAGE C;"
 
-gcc -I $INCLUDEDIR -fpic -c add.c -I $LIBDIR -lhstore -O3 -march=native
+gcc -I $INCLUDEDIR -fpic -c add.c -I $LIBDIR -lhstore -O2 -march=native
 gcc -shared -o add.so add.o
 psql -U postgres -h localhost $DB -c "CREATE OR REPLACE FUNCTION welle_add(a hstore, b hstore) RETURNS hstore AS '$WORKDIR/add.so' LANGUAGE C;"
 psql -U postgres -h localhost $DB -c "DROP AGGREGATE IF EXISTS welle_sum(hstore);"
 psql -U postgres -h localhost $DB -c "CREATE AGGREGATE welle_sum ( sfunc = welle_add, basetype = hstore, stype = hstore, initcond = '');"
 
-gcc -I $INCLUDEDIR -fpic -c uniq.c -I $LIBDIR -O3 -march=native
+gcc -I $INCLUDEDIR -fpic -c uniq.c -I $LIBDIR -O2 -march=native
 gcc -shared -o uniq.so uniq.o
 psql -U postgres -h localhost $DB -c "CREATE OR REPLACE FUNCTION roa_uniq(integer[]) RETURNS integer[] AS '$WORKDIR/uniq.so' LANGUAGE C;"
 psql -U postgres -h localhost $DB
